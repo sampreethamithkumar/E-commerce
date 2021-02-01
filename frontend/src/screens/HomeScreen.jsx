7eff9b41022778073bd1f829d1646f09bda16a5c
sami0013 <sami0013@student.monash.edu>
2021-02-02 10:13:41 +1100

    Mongo Connection to atlas added

---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,33 @@
+import React, { Fragment, useState, useEffect } from "react";
+import { Row, Col } from "react-bootstrap";
+import axios from "axios";
+import Prodcut from "./../components/Prodcut";
+
+const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products");
+
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <Fragment>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+            <Prodcut product={product} />
+          </Col>
+        ))}
+      </Row>
+    </Fragment>
+  );
+};
+
+export default HomeScreen;
