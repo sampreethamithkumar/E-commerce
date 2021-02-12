1da9e8b02f1e294ea9dfe143cf4def5d1b191a17
sami0013 <sami0013@student.monash.edu>
2021-02-12 11:55:25 +1100

    Fetching User details with a protected route

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,6 +1,6 @@
-import { CART_ADD_ITEM } from "../constants/cartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
 
-export const cartReducer = (state = { cartItems: [] }, action) => {
+export const cartReducer = (state = { cartItem: [] }, action) => {
   switch (action.type) {
     case CART_ADD_ITEM:
       const item = action.payload;
@@ -10,7 +10,7 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
       if (existItem) {
         return {
           ...state,
-          cartItem: state.cartItems.map((x) =>
+          cartItem: state.cartItem.map((x) =>
             x.product === existItem.product ? item : x
           ),
         };
@@ -20,7 +20,11 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
           cartItem: [...state.cartItem, item],
         };
       }
-
+    case CART_REMOVE_ITEM:
+      return {
+        ...state,
+        cartItem: state.cartItem.filter((x) => x.product !== action.payload),
+      };
     default:
       return state;
   }
